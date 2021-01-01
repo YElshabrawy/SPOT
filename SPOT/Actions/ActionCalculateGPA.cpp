96f36ab3bebf5c5d37a86d88978d77282d10c6d3
Roon311 <Roon311@users.github.com>
2021-01-01 06:19:57 +0200

    GPA variables added to header

--- SPOT/Actions/ActionCalculateGPA.cpp
+++ SPOT/Actions/ActionCalculateGPA.cpp
@@ -8,25 +8,10 @@
 ActionCalculateGPA::ActionCalculateGPA(Registrar* P) :Action(P)
 {
 }
-double total = 0;
-int totaldonehours = 0;
-double GPA;
 
 bool ActionCalculateGPA::Execute()
 {
-	cout << "nvnnkdn"<<endl;
-	//enum Grade:double{A=4,Amin=3.7,B};
-	const double A = 4.0;
-	const double A_MINUS = 3.67;
-	const double B_PLUS = 3.33;
-	const double B = 3.0;
-	const double B_MINUS = 2.67;
-	const double C_PLUS = 2.33;
-	const double C = 2.0;
-	const double C_MINUS = 1.67;
-	const double F = 0.0;
-
-	StudyPlan* plan = pReg->getStudyPlay();
+		StudyPlan* plan = pReg->getStudyPlay();
 	vector<AcademicYear*>* pPlan = plan->getStudyPlanVector(); // pointer on the plan vector
 	for (AcademicYear* yr : *pPlan)
 	{
@@ -88,7 +73,10 @@ bool ActionCalculateGPA::Execute()
 	cout << GPA;
 	return true;
 }
-
+double ActionCalculateGPA::getGPA() const
+{
+	return GPA;
+}
 ActionCalculateGPA::~ActionCalculateGPA()
 {
 }
--- SPOT/Actions/ActionCalculateGPA.h
+++ SPOT/Actions/ActionCalculateGPA.h
@@ -4,8 +4,22 @@
 class ActionCalculateGPA :
 	public Action
 {
+private:
+	double GPA;
+	const double A = 4.0;
+	const double A_MINUS = 3.67;
+	const double B_PLUS = 3.33;
+	const double B = 3.0;
+	const double B_MINUS = 2.67;
+	const double C_PLUS = 2.33;
+	const double C = 2.0;
+	const double C_MINUS = 1.67;
+	const double F = 0.0;
+	double total = 0;
+	int totaldonehours = 0;
 public:
 	ActionCalculateGPA(Registrar*);
 	bool virtual Execute();
+	double getGPA() const;
 	virtual ~ActionCalculateGPA();
 };
