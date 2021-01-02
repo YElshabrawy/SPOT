81cceedfd49fbe0d05279b95b3b01cdbe3611d64
Roon311 <Roon311@users.github.com>
2021-01-02 19:33:31 +0200

    declare major editing

--- SPOT/Actions/ActionDeclareMajor.cpp
+++ SPOT/Actions/ActionDeclareMajor.cpp
@@ -12,9 +12,43 @@ ActionDeclareMajor::ActionDeclareMajor(Registrar* p) :Action(p)
 
 bool ActionDeclareMajor::Execute()
 {
-int	Sci_Flag = 0, Eng_Flag = 0;
+int	Sci_Flag = 0, Eng_Flag = 0,DoubleMajor_flag=0,SingleMajor_flag=0;
 	cout << "Declaring a major.\n";
-	while ((pMajorWind)&&((Sci_Flag!=1)||(Eng_Flag!=1))) {
+	while ((pMajorWind) && ((DoubleMajor_flag != 1) || (SingleMajor_flag != 1)))
+	{
+		clicktype ctInput;
+		int x, y;
+		pMajorWind->GetMouseCoord(x, y);
+			if ((pMajorWind->GetGreen(x, y) == 0.8) && (pMajorWind->GetRed(x, y) == 0.8) && (pMajorWind->GetBlue(x, y) == 0.8))//if user clicked white area
+			{
+				delete pMajorWind;
+				pMajorWind = nullptr;
+				break;
+			}
+			pMajorWind->DrawImage("GUI\\Images\\Major_img\\DoubleMajor.jpg", MajorWindWidth / 8, MajorWindWidth / 4 - 10, MajorWindWidth / 4, MajorWindWidth / 4);
+			pMajorWind->DrawImage("GUI\\Images\\Major_img\\DeclareSingleMajor.jpg", MajorWindWidth * (5.0 / 8), 0, MajorWindWidth / 4, MajorWindWidth /1.5);
+			ctInput = pMajorWind->GetMouseClick(x, y);
+			if (ctInput == 1) {
+				pMajorWind->GetMouseCoord(x, y);
+
+			if ((x >= (MajorWindWidth / 8)) && (x <= ((MajorWindWidth / 8) + (MajorWindWidth / 4))) && (y >= (MajorWindWidth / 4 - 10)) && (y <= (MajorWindWidth / 4 - 10) + (MajorWindWidth / 4)))
+			{
+				DoubleMajor_flag = 1;
+				UpdateWind();
+				break;
+			}
+			if ((x >= MajorWindWidth * (5.0 / 8)) && (x <= ((MajorWindWidth * (5.0 / 8)) + (MajorWindWidth / 4))) && (y >= (MajorWindWidth / 4 - 10)) && (y <= (MajorWindWidth / 4 - 10) + (MajorWindWidth / 4)))
+			{
+				SingleMajor_flag = 1;
+				UpdateWind();
+				break;
+			}
+			if (pMajorWind->GetMouseClick(x, y))
+				UpdateWind();
+
+		}
+	}
+	while ((pMajorWind)&&((Sci_Flag!=1)||(Eng_Flag!=1)) && ((DoubleMajor_flag == 1) || (SingleMajor_flag ==1))) {
 		clicktype ctInput;
 		int x, y;
 		pMajorWind->GetMouseCoord(x, y);
